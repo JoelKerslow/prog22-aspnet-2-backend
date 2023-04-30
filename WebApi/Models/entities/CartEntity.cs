5af0fa16333ed7fd7456221fcfac44b07e6ce8f9
Acensti <37810260+Acensti@users.noreply.github.com>
2023-04-30 20:06:45 +0200

    small changes

--- WebApi/Models/entities/CartEntity.cs
+++ WebApi/Models/entities/CartEntity.cs
@@ -1,6 +1,23 @@
-﻿namespace WebApi.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Entities
 {
     public class CartEntity
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public ProductEntity Product { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public CustomerProfileEntity Customer { get; set; }
     }
 }
--- WebApi/Models/entities/IdentityUserEntity.cs
+++ WebApi/Models/entities/IdentityUserEntity.cs
@@ -9,7 +9,6 @@ namespace WebApi.Models.Entities
         [Required]
         public string UserName { get; set; }
         [Required]
-
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
