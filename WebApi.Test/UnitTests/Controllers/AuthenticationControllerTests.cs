d47f87bb9bb8a040911be139cd893b418bd87683
AleDotz <114862977+AleDotz@users.noreply.github.com>
2023-05-31 15:40:19 +0200

    Ales unit tests "Register"

---
+++ WebApi.Test/UnitTests/Controllers/AuthenticationControllerTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using WebApi.Helpers.Services;
+using WebApi.Interfaces;
+using WebApi.Models.Schemas;
+
+namespace WebApi.Test.UnitTests.Controllers
+{
+    public class AuthenticationControllerTests
+    {
+        private Mock<IAuthService> _authServiceMock;
+        private AuthenticationController _controller;
+
+        public AuthenticationControllerTests()
+        {
+            _authServiceMock = new Mock<IAuthService>();
+            _controller = new AuthenticationController(_authServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task Register_ShouldRegisterNewUser_ReturnsCreated()
+        {
+            //Arrange
+            var schema = new CustomerRegisterSchema
+            {
+                FirstName = "Olle",
+                LastName = "Hammarsson",
+                Email = "olle@domain.com",
+                Password = "password"
+            };
+
+            _authServiceMock.Setup(s => s.RegisterAsync(schema)).ReturnsAsync(true);
+
+            //Act
+            var result = await _controller.Register(schema);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        [Fact]
+        public async Task Register_ShouldReturnConflict_ReturnsConflict()
+        {
+            //Arrange
+            var schema = new CustomerRegisterSchema
+            {
+                LastName = "Hammarsson",
+                Email = "olle@domain.com",
+            };
+            _authServiceMock.Setup(s => s.RegisterAsync(schema));
+
+            //Act
+            var result = await _controller.Register(schema);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<ConflictResult>(result);
+        }
+
+        [Fact]
+        public async Task Register_ShouldReturnBadRequest_ReturnsBadRequest()
+        {
+            // Arrange
+            var schema = new CustomerRegisterSchema
+            {
+                FirstName = "Olle",
+                LastName = "Hammarsson"
+            };
+            _controller.ModelState.AddModelError("ModelError", "Model state not valid");
+
+            // Act
+            var result = await _controller.Register(schema) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(400, result.StatusCode);
+        }
+
+    }
+}
--- WebApi/Helpers/Services/AuthService.cs
+++ WebApi/Helpers/Services/AuthService.cs
@@ -1,59 +1,60 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using WebApi.Interfaces;
 using WebApi.Models.Schemas;
 
 namespace WebApi.Helpers.Services;
 
-public class AuthService
+public class AuthService : IAuthService
 {
-	private readonly UserManager<IdentityUser> _userManager;
-	private readonly SignInManager<IdentityUser> _signInManager;
-	private readonly CustomerProfileService _customerProfileService;
-	private readonly JwtService _jwtService;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly CustomerProfileService _customerProfileService;
+    private readonly JwtService _jwtService;
 
-	public AuthService(UserManager<IdentityUser> userManager, CustomerProfileService customerProfileService, SignInManager<IdentityUser> signInManager, JwtService jwtService)
-	{
-		_userManager = userManager;
-		_customerProfileService = customerProfileService;
-		_signInManager = signInManager;
-		_jwtService = jwtService;
-	}
+    public AuthService(UserManager<IdentityUser> userManager, CustomerProfileService customerProfileService, SignInManager<IdentityUser> signInManager, JwtService jwtService)
+    {
+        _userManager = userManager;
+        _customerProfileService = customerProfileService;
+        _signInManager = signInManager;
+        _jwtService = jwtService;
+    }
 
-	public async Task<bool> RegisterAsync(CustomerRegisterSchema schema)
-	{
-		var identityResult = await _userManager.CreateAsync(schema, schema.Password);
-		if (identityResult.Succeeded)
-		{
-			var user = await _userManager.FindByEmailAsync(schema.Email);
-			var customerProfileEntity = await _customerProfileService.CreateAsync(schema, user!.Id);
+    public async Task<bool> RegisterAsync(CustomerRegisterSchema schema)
+    {
+        var identityResult = await _userManager.CreateAsync(schema, schema.Password);
+        if (identityResult.Succeeded)
+        {
+            var user = await _userManager.FindByEmailAsync(schema.Email);
+            var customerProfileEntity = await _customerProfileService.CreateAsync(schema, user!.Id);
 
-			if (customerProfileEntity != null)
-				return true;
+            if (customerProfileEntity != null)
+                return true;
 
-			await _userManager.DeleteAsync(user);
-		}
+            await _userManager.DeleteAsync(user);
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public async Task<string> LoginAsync(CustomerLoginSchema schema)
-	{
-		var identityUser = await _userManager.FindByEmailAsync(schema.Email);
-		if (identityUser != null)
-		{
-			var signInResult = await _signInManager.CheckPasswordSignInAsync(identityUser, schema.Password, false);
-			if (signInResult.Succeeded)
-			{
-				var claimsIdentity = new ClaimsIdentity(new Claim[]
-				{
-					new Claim("id", identityUser.Id),
-					new Claim(ClaimTypes.Name, identityUser.Email!)
-				});
+    public async Task<string> LoginAsync(CustomerLoginSchema schema)
+    {
+        var identityUser = await _userManager.FindByEmailAsync(schema.Email);
+        if (identityUser != null)
+        {
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(identityUser, schema.Password, false);
+            if (signInResult.Succeeded)
+            {
+                var claimsIdentity = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("id", identityUser.Id),
+                    new Claim(ClaimTypes.Name, identityUser.Email!)
+                });
 
-				return _jwtService.Generate(claimsIdentity, DateTime.Now.AddDays(1));
-			}
-		}
+                return _jwtService.Generate(claimsIdentity, DateTime.Now.AddDays(1));
+            }
+        }
 
-		return string.Empty;
-	}
+        return string.Empty;
+    }
 }
